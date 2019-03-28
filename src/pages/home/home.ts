91bc0643021e2f40db8bfce62238f6e5b64b47a7
bastok <bfromager@gmail.com>
2019-03-28 12:24:16 +0100

    Get image from internet

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {HttpService} from '../../services/http.service';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    httpString : string = "tata";
+    imgSrc="http://fail18.com/upload/20190327154529uid1.jpg";
 
-  }
+    constructor(public navCtrl: NavController, private httpService: HttpService) {
+
+        //this.httpService.getContent('https://api.github.com/users')
+         this.httpService.getContent('http://www.bastok.fr/oops/?page=1&json')
+            .then((data: string[]) => {
+                    console.log(data);
+                    this.httpString = data[0];
+                    this.imgSrc = data[0];
+                }
+            )
+            .catch(
+                (error: string) => {this.httpString = "error : " + error}
+            );
+    }
+
+
+
 
 }
